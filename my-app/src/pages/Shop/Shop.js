e6b86baa82bf921c17e34bb245e6c183b2763bf7
hawatr <robertohawat03@gmail.com>
2024-01-01 12:15:23 +0200

    total

--- my-app/src/pages/Shop/Shop.js
+++ my-app/src/pages/Shop/Shop.js
@@ -5,10 +5,15 @@ import Axios from 'axios';
 import "./shop.css";
 function Shop() {
   const location = useLocation();
+  const [menuList, setList] = useState([]);
+
   const [ingList, setIngList] = useState([]);
   const name = new URLSearchParams(location.search).get('name');
   console.log(name);
+  const [total, setTotal] = useState(0);
+  const [total2, setTotal2] = useState(0);
   const submitR = () => {
+    
     Axios.post('http://localhost:3001/api/setOrdered', { username: name })
       .then(() => {
         alert('Successful insert');
@@ -22,17 +27,27 @@ function Shop() {
   }, []);
 
   useEffect(() => {
+    // Calculate the total whenever menuList changes
+    const newTotal = menuList.reduce((acc, item) => acc + item.price * item.quantity, 0);
+    setTotal(newTotal);
+  }, [menuList]);
+
+  useEffect(() => {
     Axios.post('http://localhost:3001/api/orderItemsIngredients', { name }).then((response) => {
       setIngList(response.data);
       console.log(response.data);
     });
   }, []);
+    useEffect(() => {
+    // Calculate the total whenever menuList changes
+    const newTotal = ingList.reduce((acc, item) => acc + item.price * item.quantity, 0);
+    setTotal2(newTotal);
+  }, [ingList]);
 
   const handleFormSubmit = async (e) => {
     // Add form submission logic here if needed
   };
 
-  const [menuList, setList] = useState([]);
 
   return (
     <div>
@@ -45,6 +60,7 @@ function Shop() {
             </p>
           </div>
         ))}
+        Total From Menu: ${total}
         <br/>
 <h1>order from ingredient</h1>
 {ingList.map((item, index) => (
@@ -54,8 +70,10 @@ function Shop() {
             </p>
           </div>
         ))}
+        Total From Ingredient: ${total2}
         <br/>
         <div className="footer">
+        Total: ${total+total2}
           <p>
           <Link to={{ pathname: '/' }} className="create-your-own-link">
   <input type="submit" value="Submit" onClick={submitR} />
